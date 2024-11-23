d8c24a417d8b08e1bb8a1b2505a504c740b4e439
AnshulVermaQ <vermaanshul975@gmail.com>
2024-11-23 14:34:43 +0530

    Initial commit

---
+++ src/redux/Store.jsx
@@ -0,0 +1,11 @@
+// redux/store.js
+import { configureStore } from '@reduxjs/toolkit';
+import appSlice from './appSlice';
+
+const store = configureStore({
+  reducer: {
+    app: appSlice,
+  },
+});
+
+export default store;
